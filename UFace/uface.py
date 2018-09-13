c7f82af65279405379f88b43ef209095cc43afc0
majiabo <birkhoffkiki@gmail.com>
2018-09-13 14:50:49 +0800

    finish gui

--- UFace/uface.py
+++ UFace/uface.py
@@ -8,7 +8,7 @@ from tkinter import filedialog
 from keras.models import load_model
 
 canvas_plot_image = None
-model_path = 'model_00.h5'
+model_path = 'F:/DataPool/UFace_data/model_00.h5'
 emotion = {0:'anger',1:'disgust',2:'fear',
             3:'happy',4:'sad',5:'surprised',6:'normal'}
 model = load_model(model_path)
@@ -16,29 +16,39 @@ class GUI():
     def __init__(self,width,height):
         self.window_width = width
         self.window_height = height
+        self.emo_class_info = "Not Detect Yet!"
         self.gui = tk.Tk()
         self.gui.title('UFace')
         self.board = tk.Canvas(self.gui,width=self.window_width,height=self.window_height)
         self.gui_init()
     def gui_init(self):
+        self.g_label()
         self.g_button()
-        self.board.pack()
         self.g_menu()
+        self.welcome_label.pack()
+        self.expression_info_label.pack()
+        self.board.pack()
 
     def open_image(self):
-        filename = filedialog.askopenfilename(initialdir='F:/')
+        filename = filedialog.askopenfilename(initialdir='F:/DataPool/')
         global canvas_plot_image
         canvas_plot_image = tk.PhotoImage(file = filename)
-        width = canvas_plot_image.width()
-        height = canvas_plot_image.height()
+        #width = canvas_plot_image.width()
+        #height = canvas_plot_image.height()
+        """
         if width>self.window_width or height > self.window_height:
             if width/self.window_width >height/self.window_height:
                 canvas_plot_image.zoom(x = self.window_width/width)
             else:
                 canvas_plot_image.zoom(x = self.window_height/height)
-        x = int((self.window_height-width)/2)
-        y = int((self.window_height-height)/2)
-        self.board.create_image(x,y,anchor = tk.CENTER,image = canvas_plot_image)
+        """
+        #x = int((self.window_height-width)/2)
+        #y = int((self.window_height-height)/2)
+        from skimage.io import imread
+        
+        self.meta_image = imread(filename)
+        self.board.create_image(200,50,anchor = 'nw',image = canvas_plot_image)
+        self.start_predict_button.pack()
     def g_menu(self):
         self.menubar = tk.Menu(self.gui)
         filemenu = tk.Menu(self.menubar,tearoff = 0)
@@ -50,17 +60,23 @@ class GUI():
     def show(self):
         self.gui.mainloop()
     def g_button(self):
-        self.start_predict_button = tk.Button(master=self.gui,text = "start predict",command = self.predict).pack()
+        self.start_predict_button = tk.Button(master=self.gui,text = "start predict",command = self.predict)
+    def g_label(self):
+        self.expression_info_label = tk.Label(master=self.gui,text = "Tips: Click File to Open File")
+        self.welcome_label = tk.Label(master=self.gui,text = "Welcome Using UFace :)")
     def predict(self):
         """
         输入表情，返回表情名字，字符串。
         """
-        img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-        img = cv2.resize(img,(48,48),interpolation=cv2.INTER_CUBIC)
+        shape = self.meta_image.shape
+        img = cv2.resize(self.meta_image,(48,48),interpolation=cv2.INTER_CUBIC)/255.
+        if len(shape)>2:
+            img = cv2.cvtColor(img , cv2.COLOR_RGB2GRAY)
         img = img.reshape([1,48,48,1])
         r = model.predict(img)
         status = np.argmax(r[0])
-        emo = emotion[status]
-        return emo
-gui = GUI(800,600)
+        self.emo_class_info = emotion[status]
+        self.expression_info_label['text'] = "这个人看起来好像很 "+ self.emo_class_info + "!"
+
+gui = GUI(800,400)
 gui.show()
